ecdf69ab54b006d18d5ee6ab2e6ecac97428a1f6
Rea6913 <47655248+Rea6913@users.noreply.github.com>
2019-02-15 22:28:31 +0300

    123
    123

--- Population.cs
+++ Population.cs
@@ -6,8 +6,16 @@ namespace PadawansTask1
     {
         public static int GetYears(int initialPopulation, double percent, int visitors, int currentPopulation)
         {
-            // put your code here
-            throw new NotImplementedException();
+            if (initialPopulation <= 0 || percent <= 0 || currentPopulation <= 0)
+                throw new ArgumentException();
+            if (initialPopulation == Int32.MaxValue || percent == Int32.MaxValue || currentPopulation == Int32.MaxValue)
+                throw new OverflowException(); 
+            int god = 0;
+            for (; currentPopulation > initialPopulation; god++) 
+            {
+                initialPopulation = Convert.ToInt32(initialPopulation * (percent / 100) + initialPopulation+ visitors);
+            }
+            return god;
         }
     }
 }
